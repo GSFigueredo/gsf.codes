53c3ae8ee80dd117ba1ed75c9fba3274f6ae9790
Gabriel Figueredo <gabriel.sfigueredo8@gmail.com>
2023-04-12 19:59:55 +0000

    6d58e290361ab2d8db009a8f4ed05f2020c4a23d

---
+++ BF-STORE/index.js
@@ -0,0 +1,39 @@
+var comp = window.document.querySelector('img#comprar')
+function comprar() {
+    comp.src = 'imagens/comprar.png'
+}
+
+function sair() {
+    comp.src = 'imagens/logocomprar.png'
+}
+
+function clicar() {
+
+}
+
+var ent = window.document.querySelector('img#ent')
+function entE() {
+    ent.src = 'imagens2/fed2.png'
+}
+
+function entS() {
+    ent.src = 'imagens2/feed.png'
+}
+
+var fed = window.document.querySelector('img#fed')
+function fedE() {
+    fed.src = 'imagens2/coment.png'
+}
+
+function fedS() {
+    fed.src = 'imagens2/feed.png'
+}
+
+var elas = window.document.querySelector('img#elas')
+function elasE() {
+    elas.src = 'imagens2/elas.png'
+}
+
+function elasS() {
+    elas.src = 'imagens/elasusam.png'
+}
